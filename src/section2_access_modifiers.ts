f447466ba6f2c5337dbfd8f994ecdc18dc33501b
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-16 08:50:24 -0400

    Build Account class and methods

--- src/section2_access_modifiers.ts
+++ src/section2_access_modifiers.ts
@@ -6,3 +6,24 @@
 // Add a method `deposit(amount: number)` to increase the balance
 
 // Your code here 👇
+class Account {
+  public owner: string;
+  private balance: number;
+  readonly id: number;
+
+  constructor(owner: string, id: number, balance: number) {
+    this.owner = owner;
+    this.id = id;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount > 0) {
+      this.balance += amount;
+    }
+  }
+
+  getBalance(): number {
+    return this.balance;
+  }
+}
