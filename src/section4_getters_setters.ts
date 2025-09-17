4acc70e5968148c7143d7b928a0ab3d35f7b67fd
Mike <mclancy96@gmail.com>
2025-09-17 09:06:18 -0400

    1-5

--- src/section4_getters_setters.ts
+++ src/section4_getters_setters.ts
@@ -6,3 +6,26 @@
 // Add a `set width(value)` that sets width only if it's > 0
 
 // Your code here 👇
+class Rectangle {
+  _width: number
+  height: number
+
+  constructor(width: number, height: number) {
+    this._width = width
+    this.height = height
+  }
+
+  get area(): number {
+    return Number((this._width * this.height).toFixed(2))
+  }
+
+  get width(): number {
+    return this._width
+  }
+
+  set width(value: number) {
+    if (value > 0) {
+      this._width = value
+    }
+  }
+}
