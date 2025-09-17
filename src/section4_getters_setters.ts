c5d0b41f2fbd71f7e83ed7e088fc545606aed6ef
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-17 00:20:16 -0400

    Create class Rectangle and write getters and setters

--- src/section4_getters_setters.ts
+++ src/section4_getters_setters.ts
@@ -6,3 +6,27 @@
 // Add a `set width(value)` that sets width only if it's > 0
 
 // Your code here 👇
+
+class Rectangle {
+  public _width: number
+  private height: number
+
+  constructor(width: number, height: number) {
+    this._width = width
+    this.height = height
+  }
+
+  public get width(): number {
+    return this._width
+  }
+
+  public set width(theWidth: number) {
+    if (theWidth > 0) {
+      this._width = theWidth
+    }
+  }
+
+  get area(): number {
+    return parseFloat((this._width * this.height).toFixed(2))
+  }
+}
