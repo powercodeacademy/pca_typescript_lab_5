ecfdcec2259b2d7a8378c6967add3336ac6eafdf
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-16 22:20:53 -0400

    Create class and method using access modifiers

--- src/section1_classes.ts
+++ src/section1_classes.ts
@@ -5,3 +5,17 @@
 // Add a method `greet()` that returns "Hi, I'm <name>"
 
 // Your code here 👇
+
+class Person {
+  name: string
+  age: number
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  greet(): string {
+    return `Hi, I'm ${this.name}`
+  }
+}
